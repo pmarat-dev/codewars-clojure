87c56ef04a5e1acf079a50799b72589f08e4fc3b
Piotr <pmarat.dev@gmail.com>
2023-11-29 00:47:09 +0100

    Cleanup.

--- src/codewars/kyu8/count_lowercase_letters.clj
+++ src/codewars/kyu8/count_lowercase_letters.clj
@@ -1,5 +1,5 @@
-; https://www.codewars.com/kata/56a946cd7bd95ccab2000055
 ; Regex count lowercase letters
+; https://www.codewars.com/kata/56a946cd7bd95ccab2000055
 
 (defn lowercase_count [str]
   (count (re-seq #"[a-z]" str)))
